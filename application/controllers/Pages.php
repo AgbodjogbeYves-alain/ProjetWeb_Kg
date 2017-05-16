f36ad7209c94f3a3f9dac719e7154c2ff2da35cf
Sasugeta <agboniel@yahoo.fr>
2017-05-16 09:46:09 +0200

    fix bug with images

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,15 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
-    public function __construct() 
+    public function __construct()
     {
         parent::__construct();
-        $this->load->helper('url');
+
     }
 
     public function accueil()
     {
       $data['title'] = ucfirst('KGE-Accueil'); // Capitalize the first letter
+      $this->load->helper('url');
       $this->load->view('template/header', $data);
       $this->load->view('pages/accueil/navbar_accueil', $data);
       $this->load->view('pages/accueil/accueil', $data);
