59f96fbf58ac59e392cdeafd14ef0813b6dc820c
root <agboniel@yahoo.fr>
2017-05-17 01:42:10 +0200

    edit test

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,13 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-        //$this->load->database();
-
-    }
-
     public function accueil()
     {
       $data['title'] = ucfirst('KGE-Accueil'); // Capitalize the first letter
@@ -21,8 +14,12 @@ class Pages extends CI_Controller {
 
     public function test(){
       $this->load->model('entreprise');
-      $data['nom_entreprise'] = $this->entreprise->get_name_entreprise(0);
-      echo($data['nom_entreprise']);
+      //$data['nom_entreprise'] = $this->entreprise->get_name_entreprise(0);
+      /*echo($data['nom_entreprise']);*/
+      $this->load->view('template/header', $data);
+      $this->load->view('pages/accueil/navbar_accueil', $data);
+      $this->load->view('pages/accueil/accueil', $data);
+      $this->load->view('template/footer', $data);
 
     }
 }
---
+++ script_remplissage.sql
@@ -0,0 +1,2 @@
+INSERT INTO 'Entreprise'('id_entreprise','nom_entreprise','descriptif_entreprise','representant','email_entreprise') VALUES (0,'Prima_center','Centre commercial','moi','prima_center@yahoo.fr');
+INSERT INTO 'Client'('num_client','password_client','type_client','email_client') VALUES (0,'toi',1,'prima_center@yahoo.fr');
