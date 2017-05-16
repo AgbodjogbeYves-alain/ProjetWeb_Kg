f4ab5df1eea091d12dfeb3c3882eb5ea2305c329
Sasugeta <agboniel@yahoo.fr>
2017-05-16 04:12:09 +0200

    affichage page accueil

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,12 +1,14 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Pages extends CI_Controller {
 
     public function accueil()
     {
       $data['title'] = ucfirst('KGE-Accueil'); // Capitalize the first letter
       $this->load->view('templates/header', $data);
-      $this->load->view('pages/navbar_'.$page, $data);
-      $this->load->view('pages/'.$page, $data);
+      $this->load->view('pages/navbar_accueil', $data);
+      $this->load->view('pages/accueil', $data);
       $this->load->view('templates/footer', $data);
     }
 }
