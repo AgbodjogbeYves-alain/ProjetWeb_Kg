ddbd9ca527aef856ead607b9617d8f87586e2a6b
Sasugeta <agboniel@yahoo.fr>
2017-05-16 04:08:12 +0200

    affichage page accueil

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,14 +1,9 @@
 <?php
 class Pages extends CI_Controller {
 
-    public function view($page = 'accueil')
+    public function accueil()
     {
-      if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')){
-                // Whoops, we don't have a page for that!
-                echo('Whoops');
-                show_404();
-        }
-      $data['title'] = ucfirst($page); // Capitalize the first letter
+      $data['title'] = ucfirst('KGE-Accueil'); // Capitalize the first letter
       $this->load->view('templates/header', $data);
       $this->load->view('pages/navbar_'.$page, $data);
       $this->load->view('pages/'.$page, $data);
