f9c636ec8f28b0a40c2cb63092290b127f7b86bb
root <agboniel@yahoo.fr>
2017-05-17 01:57:32 +0200

    edit test

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
+  public function __construct()
+     {
+             parent::__construct();
+             $this->load->helper('url_helper');
+     }
+
     public function accueil()
     {
       $data['title'] = ucfirst('KGE-Accueil'); // Capitalize the first letter
