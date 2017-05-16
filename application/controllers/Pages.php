bb1b94265335c9b0f60db798e631dfefb089ed99
Sasugeta <agboniel@yahoo.fr>
2017-05-16 04:16:17 +0200

    affichage page accueil

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,10 +6,10 @@ class Pages extends CI_Controller {
     public function accueil()
     {
       $data['title'] = ucfirst('KGE-Accueil'); // Capitalize the first letter
-      $this->load->view('templates/header', $data);
+      $this->load->view('template/header', $data);
       $this->load->view('pages/navbar_accueil', $data);
       $this->load->view('pages/accueil', $data);
-      $this->load->view('templates/footer', $data);
+      $this->load->view('template/footer', $data);
     }
 }
 ?>
