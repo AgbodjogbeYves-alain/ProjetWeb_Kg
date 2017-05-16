da78916f3a7870730bc2370421dd43d6ed83e0b6
Sasugeta <agboniel@yahoo.fr>
2017-05-16 03:52:33 +0200

    add composer

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -5,6 +5,7 @@ class Pages extends CI_Controller {
     {
       if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')){
                 // Whoops, we don't have a page for that!
+                echo('Whoops');
                 show_404();
         }
       $data['title'] = ucfirst($page); // Capitalize the first letter
