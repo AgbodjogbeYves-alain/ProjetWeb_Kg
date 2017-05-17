ffae90b450200417479f3b588f28e92b5d08e339
root <agboniel@yahoo.fr>
2017-05-17 03:18:36 +0200

    edit test

--- application/models/Client_model.php
+++ application/models/Client_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Entreprise_model extends CI_Model {
+class Client_model extends CI_Model {
 
         public function __construct() {
                 // Call the CI_Model constructor
