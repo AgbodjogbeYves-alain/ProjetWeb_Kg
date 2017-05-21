0387616d61e2476a5f6e1ae6923897ff631a8ca9
sasugeta <agboniel@yahoo.fr>
2017-05-21 03:19:05 +0200

    add navbar admin

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
   <!-- Site Properties -->
-  <link rel="stylesheet" type="text/css" href="<?php  echo base_url("element_design/semantic/out/semantic.min.css");?>"/>
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url("element_design/css/mystyle.css");?>"/>
+  <link rel="stylesheet" type="text/css" href="element_design/semantic/out/semantic.min.css"/>
+  <link rel="stylesheet" type="text/css" href="element_design/css/mystyle.css"/>
   <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-  <script src="<?php echo base_url("element_design/semantic/out/semantic.min.js");?>"></script>
+  <script src="element_design/semantic/out/semantic.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
 
 </head>
 
 <body ng-app="kge-project">
-  <script src="<?php echo base_url("js/app.js");?>"></script>
+  <script src="element_design/js/app.js"></script>
 
 	<div class="pusher" >
